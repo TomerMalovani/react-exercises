2e2acf319dcce663b619f5d3cc06a0124b62fe92
Tomer Malovani <tomermalovani@gmail.com>
2019-06-23 14:58:58 +0300

    star 3

--- day1/ex3/logic.js
+++ day1/ex3/logic.js
@@ -1,4 +1,39 @@
+class RandomBtn extends React.Component {
+    render() {
+        return (
+            <div>
+                <input type="button" className="button" value="click"></input>
+            </div>
+
+        );
+    }
+}
+
+
+class RandomName extends React.Component {
+    render() {
+        return (
+            <div className="Box">
+                <RandomBtn />
+            </div>
+
+        );
+    }
+}
+
+
+class Page extends React.Component {
+    render() {
+        return (
+            <div className="page">
+                <RandomName />
+            </div>
+
+        );
+    }
+}
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <Page />,
     document.getElementById("root")
 );
