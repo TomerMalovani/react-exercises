745592ffdef97b3a2f749595af7e3571265a3e6d
Tomer Malovani <tomermalovani@gmail.com>
2019-06-24 19:37:06 +0300

    star2

--- day1/ex2/logic.js
+++ day1/ex2/logic.js
@@ -3,10 +3,17 @@ class ColThree extends React.Component {
         return (
             <div className="Col">
                 <ImgThree />
-                <p className="SubTxt">user interface design to make real people feel real feelings</p>
+                <p className="SubTxt" onMouseEnter={this.on} onMouseLeave={this.off}>user interface design to make real people feel real feelings</p>
             </div>
 
         );
+    }
+    on(event) {
+        event.target.classList.add("hoverP");
+    };
+
+    off(event) {
+        event.target.classList.remove("hoverP");
     }
 }
 
@@ -16,10 +23,17 @@ class ColTwo extends React.Component {
         return (
             <div className="Col">
                 <ImgTwo />
-                <p className="SubTxt">concept and idea for you to re-invent whatever u think u may</p>
+                <p className="SubTxt" onMouseEnter={this.on} onMouseLeave={this.off}>concept and idea for you to re-invent whatever u think u may</p>
             </div>
 
         );
+    }
+    on(event) {
+        event.target.classList.add("hoverP");
+    };
+
+    off(event) {
+        event.target.classList.remove("hoverP");
     }
 }
 
@@ -29,45 +43,105 @@ class ColOne extends React.Component {
         return (
             <div className="Col">
                 <ImgOne />
-                <p className="SubTxt">design brands : Nike Adidas Supra Vans</p>
+                <p className="SubTxt" onMouseEnter={this.on} onMouseLeave={this.off}>design brands : Nike Adidas Supra Vans</p>
             </div>
 
         );
+    }
+    on(event) {
+        event.target.classList.add("hoverP");
+    };
+
+    off(event) {
+        event.target.classList.remove("hoverP");
     }
 }
 
 class ImgThree extends React.Component {
+    constructor() {
+        super()
+        this.on = this.on.bind(this);
+        this.off = this.off.bind(this);
+
+    }
     render() {
         return (
-            <div className="Img Three">
+            <div className="Img Three" id="img3" onMouseEnter={this.on} onMouseLeave={this.off}>
 
             </div>
 
         );
+    }
+    on() {
+        console.log("hey");
+        let img = document.getElementById("img3");
+        img.className = "Three_hover";
+    };
+
+    off() {
+        let img = document.getElementById("img3");
+        img.className = "img Three";
     }
 }
 
 class ImgTwo extends React.Component {
+    constructor() {
+        super()
+        this.on = this.on.bind(this);
+        this.off = this.off.bind(this);
+
+    }
     render() {
         return (
-            <div className="Img Two">
+            <div className="Img Two" id="img2" onMouseEnter={this.on} onMouseLeave={this.off}>
 
             </div>
 
         );
+    }
+    on() {
+        console.log("hey");
+        let img = document.getElementById("img2");
+        img.className = "Two_hover";
+    };
+
+    off() {
+        let img = document.getElementById("img2");
+        img.className = "img Two";
     }
 }
 
 class ImgOne extends React.Component {
+
+    constructor() {
+        super()
+        this.on = this.on.bind(this);
+        this.off = this.off.bind(this);
+
+    }
     render() {
+
         return (
-            <div className="Img One">
+            <div className="Img One" id="img1" onMouseEnter={this.on} onMouseLeave={this.off}>
 
             </div>
 
         );
     }
+    on() {
+        console.log("hey");
+        let img = document.getElementById("img1");
+        img.className = "One_hover";
+    };
+
+    off() {
+        let img = document.getElementById("img1");
+        img.className = "img one";
+    }
 }
+
+
+
 
 
 class PicPart extends React.Component {
@@ -111,17 +185,31 @@ class MainBody extends React.Component {
 }
 
 class MenuNav extends React.Component {
+    constructor() {
+        super()
+        this.on = this.on.bind(this);
+        this.off = this.off.bind(this);
+
+    }
     render() {
+
         return (
             <div className="Menu">
-                <ul className="Menubar">
-                    <li className="Bar">services</li>
-                    <li className="Bar">work</li>
-                    <li className="Bar">the team</li>
+                <ul className="Menu" >
+                    <li className="Bar" onMouseEnter={this.on} onMouseLeave={this.off}>services</li>
+                    <li className="Bar" onMouseEnter={this.on} onMouseLeave={this.off}>work</li>
+                    <li className="Bar" onMouseEnter={this.on} onMouseLeave={this.off}>the team</li>
                 </ul>
             </div>
 
         );
+    }
+    on(event) {
+        event.target.classList.add("hoverText");
+    }
+
+    off(event) {
+        event.target.classList.remove("hoverText");
     }
 
 }
