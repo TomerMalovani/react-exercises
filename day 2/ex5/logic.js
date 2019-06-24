126ac25026ced6f2a1462ffcf351528bd363a016
Tomer Malovani <tomermalovani@gmail.com>
2019-06-24 13:52:15 +0300

    star 1 yay

--- day 2/ex5/logic.js
+++ day 2/ex5/logic.js
@@ -1,4 +1,23 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+function render(data) {
+
+    ReactDOM.render(
+        <div className="wrap">
+            <a className="WebSiteLink" style={{ color: data.color, }} href={data.linkAddress}>go to website</a> ,
+        <input type="range" min={data.min} max={data.max}></input> ,
+         <input type="button" value={data.buttonText} style={{ color: data.color }}></input> ,
+        </div>,
+
+        document.getElementById("root"));
+}
+var data = {
+    linkAddress: "https://www.google.com/",
+    linkText: "google",
+    min: -5,
+    max: 5,
+    color: "maroon",
+    "font-weight": "bold",
+    buttonText: "click me"
+};
+render(data);
+
+
