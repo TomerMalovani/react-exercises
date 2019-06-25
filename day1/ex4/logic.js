252a000fe7ae668a9bcfa70b02cb8e54a3957558
Tomer Malovani <tomermalovani@gmail.com>
2019-06-25 12:36:55 +0300

    class last call

--- day1/ex4/logic.js
+++ day1/ex4/logic.js
@@ -1,4 +1,41 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+
+class Boxitem extends React.Component {
+
+    render() {
+
+        return (
+            <div className={this.props.style + " box"}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Boxitem style="firstBox">
+                    <Boxitem style="SecBox">
+                        <Boxitem style="ThirdBox">
+                            <Boxitem style="DualBox" />
+                            <Boxitem style="DualBox" />
+                        </Boxitem>
+                    </Boxitem>
+                </Boxitem>
+
+
+
+            </div>
+        );
+    }
+}
+function render() {
+    ReactDOM.render(
+        <App />,
+        document.getElementById("root")
+    );
+}
+render();
--- day1/ex5/logic.js
+++ day1/ex5/logic.js
@@ -1,4 +1,33 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+class Box extends React.Component {
+    render() {
+        var style = {
+            "letterSpacing": this.props.space
+        };
+        return (
+            <div className={this.props.color + " box"}>
+                <h4>{this.props.text} </h4>
+            </div>
+
+
+        );
+    }
+}
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Box color="blue" text="blue" />
+                <Box color="red" text="red" />
+                <Box color="brown" text="brown" />
+                <Box color="purple" text="purple" />
+            </div>
+        );
+    }
+}
+function render() {
+    ReactDOM.render(
+        <App />,
+        document.getElementById("root")
+    );
+}
+render();
