5b3ea7f39e30c501776c3c0020de0cff43083c8d
Tomer Malovani <tomermalovani@gmail.com>
2019-06-25 10:40:54 +0300

    class star 2

--- day1/ex6/logic.js
+++ day1/ex6/logic.js
@@ -1,4 +1,31 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+class Title extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        var style = {
+            "letterSpacing": this.props.space
+        };
+        return (
+            <button style={style} >{this.props.text} </button>
+
+        );
+    }
+}
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Title text="click" space={1 + "em"} />
+                <Title text="click" space={0 + "em"} />
+            </div>
+        );
+    }
+}
+function render() {
+    ReactDOM.render(
+        <App />,
+        document.getElementById("root")
+    );
+}
+render();
