6f7087e5d90ffc214b69f67dce19529944004e04
Tomer Malovani <tomermalovani@gmail.com>
2019-06-25 10:29:13 +0300

    class fun

--- day1/ex7/logic.js
+++ day1/ex7/logic.js
@@ -1,4 +1,31 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+class Title extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        var style = {
+            "fontFamily": this.props.font
+        };
+        return (
+            <p style={style}>{this.props.text} </p>
+
+        );
+    }
+}
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Title text="If I Rule the DOM" font="cursive" />
+                <Title text="Talk is cheap. Show me the code" font="fantasy" />
+            </div>
+        );
+    }
+}
+function render() {
+    ReactDOM.render(
+        <App />,
+        document.getElementById("root")
+    );
+}
+render();
