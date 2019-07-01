3c876d786067b338cfd12abc637b8ee5a8bd1865
Tomer Malovani <tomermalovani@gmail.com>
2019-07-01 12:59:53 +0300

    star 1

---
+++ data flow/star 1/logic.js
@@ -0,0 +1,44 @@
+class Btn extends React.Component {
+    constructor() {
+        super()
+        this.childChange = this.childChange.bind(this);
+    }
+    childChange() {
+        this.props.changeColor(this.props.color);
+    }
+    render() {
+        return (
+            <button onClick={this.childChange}>{this.props.color}</button>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            Color: "white"
+        }
+        this.Changeclr = this.Changeclr.bind(this);
+    }
+    Changeclr(NewColor) {
+        this.setState({
+            Color: NewColor
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Btn color="blue" changeColor={this.Changeclr} />
+                <Btn color="green" changeColor={this.Changeclr} />
+                <Btn color="red" changeColor={this.Changeclr} />
+                <div className={"box " + this.state.Color}></div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
